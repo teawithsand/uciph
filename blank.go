143f7309c25465be94a7403ab900c6dc07004174
teawithsand <teawithsand@gmail.com>
2020-05-21 19:45:08 +0000

    Added docs on blank enc/dec

--- blank.go
+++ blank.go
@@ -1,10 +1,14 @@
 package uciph
 
+// BlankEncryptor is encryptor which is essentially NO-OP.
+// It appends in to res.
 var BlankEncryptor Encryptor = EncryptorFunc(func(in, appendTo []byte) (res []byte, err error) {
 	res = append(appendTo, in...)
 	return
 })
 
+// BlankDecryptor is decryptor which is essentially NO-OP.
+// It appends in to res.
 var BlankDecryptor Decryptor = DecryptorFunc(func(in, appendTo []byte) (res []byte, err error) {
 	res = append(appendTo, in...)
 	return
